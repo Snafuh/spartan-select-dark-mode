0bfadc2323d57b26e0b4bf7ad4d1526476caafd0
Snafuh <kim.peter.wagner@gmail.com>
2024-10-17 12:38:57 +0200

    select reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmSpinnerComponent } from '@spartan-ng/ui-spinner-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { provideIcons } from '@ng-icons/core';
+import { lucideChevronRight } from '@ng-icons/lucide';
+import { lucideChevronDown, lucideChevronUp } from '@ng-icons/lucide';
+import { BrnSelectImports } from '@spartan-ng/ui-select-brain';
+import { HlmSelectImports } from '@spartan-ng/ui-select-helm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HlmButtonDirective,
+    HlmSpinnerComponent,
+    HlmIconComponent,
+    BrnSelectImports,
+    HlmSelectImports,
+  ],
+  providers: [
+    provideIcons({ lucideChevronRight, lucideChevronUp, lucideChevronDown }),
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'spartan-test';
